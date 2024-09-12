a88f12b376810468c47fc1f39d90af1e7b4d099d
leek94 <khwan929@naver.com>
2024-09-12 19:27:03 +0900

    change adminSidebar width

--- src/components/AdminSidebar/index.vue
+++ src/components/AdminSidebar/index.vue
@@ -1,30 +1,23 @@
 <template>
   <nav class="navbar p-0">
- 
-      
+    <!-- 사이드바 -->
+    <div class="vh-100 side-bar bg-black ">
+      <div class="mt-5">
+          <h1 class="text-white p-3 border-3 border-top border-bottom">admin</h1>
 
-        <!-- 사이드바 -->
-        <div class="vh-100 side-bar bg-black">
-          <div class="mt-5">
-              <h1 class="text-white p-3 border-3 border-top border-bottom">admin</h1>
-
-              <ul class="navbar-na mt-5">
-              <li class="nav-item">
-                  <a class="nav-link text-white" href="#">Exit</a>
-              </li>
-              <li class="nav-item  mt-5">
-                  <a class="nav-link text-white" href="#">manage Images</a>
-              </li>
-              <li class="nav-item  mt-5">
-                  <a class="nav-link text-white" href="#">manage Category</a>
-              </li>
-              </ul>
-          </div>
-        </div>
-
-        <!-- 메인 콘텐츠 -->
-        
-    
+          <ul class="navbar-na mt-5">
+          <li class="nav-item">
+              <a class="nav-link text-white" href="#">Exit</a>
+          </li>
+          <li class="nav-item  mt-5">
+              <a class="nav-link text-white" href="#">manage Images</a>
+          </li>
+          <li class="nav-item  mt-5">
+              <a class="nav-link text-white" href="#">manage Category</a>
+          </li>
+          </ul>
+      </div>
+    </div>
    
   </nav>
 </template>
@@ -33,8 +26,7 @@
 </script>
 
 <style scoped>
-.side-bar {
-  min-height: 100vh; /* 최소 높이를 화면 전체로 설정 */
-  width: 100%;
+.side-bar{
+  width: 200px;
 }
 </style>
