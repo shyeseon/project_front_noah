6e30b7529b264807c2d180c54b459fada2223240
sunghwan kwon <sunghwan.ian.kwon@gmail.com>
2024-09-12 18:13:40 +0900

    updated contact,about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,11 +1,49 @@
 <template>
-    <div>
-        about
-    </div>
-</template>
-<script setup>
+  <div class="container mt-5 pt-5">
+    <div class="row">
+        <div class="col-md-4">
+        <img
+          src="../assets/image.png"
+          alt="Woman in red shirt"
+          class="img-fluid rounded"
+        />
+      </div>
+      <div class="col-md-6">
+        <h1 class="display-4 mb-4 fw-normal">Authentic Design Resources</h1>
 
+        <h2 class="h3 mb-4 fw-normal">The Challenge</h2>
+        <p class="mb-4 text-muted">
+          With our new features Cesis Studio, you can now import templates
+          directly in the page from the Frontend or Backend page builder. We
+          have created and classed templates depending on their styles and
+          purpose in order to give the user and smart interface to easily find
+          the templates that fit their needs.
+        </p>
+
+        <h3 class="h4 mb-3 fw-normal">Easily Build Pages</h3>
+        <p class="mb-4 text-muted">
+          You can customize anything you see directly from the front-end, you
+          can see the changes you make live and you don't need to code anything
+          to do it.
+        </p>
+
+        <p class="text-muted">
+          You can customize anything you see directly from the front-end, you
+          can see the changes you make live and you don't need to code anything
+          to do it.
+        </p>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<script setup>
+// 필요한 경우 여기에 컴포넌트 로직을 추가하세요
 </script>
+
 <style scoped>
-    
+body {
+  font-family: "Times New Roman", serif;
+}
 </style>
--- src/views/listpage/PhotoList.vue
+++ src/views/listpage/PhotoList.vue
@@ -4,7 +4,7 @@
     <div class="row row-cols-1 row-cols-md-3 g-4 mt-5">
       <div v-for="photo in photos" :key="photo.id" class="col">
         <RouterLink to="#" class="card h-100 border-0 text-decoration-none">
-          <img :src="photo.src" class="card-img-top" :alt="photo.title">
+          <img :src="photo.src" class="card-img-top" :alt="photo.title" />
           <div class="card-body ps-0">
             <h5 class="card-title fw-bolder">{{ photo.title }}</h5>
             <small class="text-muted font-lighter">{{ photo.category }}</small>
@@ -16,13 +16,27 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref } from "vue";
 
 //dummy data
 const photos = ref([
-  { id: 1, title: 'Liquid IV',category: 'Lifestyle', src: 'https://www.davehillphoto.com/images/pics/1536/18865_zYcW4F_20240416_hill_liquid_iv_sbc_beach_volleyball_16968.jpg' },
-  { id: 2, title: 'Ora Cat',category:'Automotive', src: 'https://www.davehillphoto.com/images/pics/1536/17197_SMt7mI_220122_dh_jvm_oracat_m96_oldcity_08_0266jpeg.jpg' },
-  { id: 3, title: 'Toyota Corolla Cross',category:'Automotive', src: 'https://www.davehillphoto.com/images/pics/1536/17145_BOBsQQ_corolla_cross_890b_08732_0031_santee_court_rear_3_4_lifestyle_004846.jpg' },
-
-])
+  {
+    id: 1,
+    title: "Liquid IV",
+    category: "Lifestyle",
+    src: "https://www.davehillphoto.com/images/pics/1536/18865_zYcW4F_20240416_hill_liquid_iv_sbc_beach_volleyball_16968.jpg",
+  },
+  {
+    id: 2,
+    title: "Ora Cat",
+    category: "Automotive",
+    src: "https://www.davehillphoto.com/images/pics/1536/17197_SMt7mI_220122_dh_jvm_oracat_m96_oldcity_08_0266jpeg.jpg",
+  },
+  {
+    id: 3,
+    title: "Toyota Corolla Cross",
+    category: "Automotive",
+    src: "https://www.davehillphoto.com/images/pics/1536/17145_BOBsQQ_corolla_cross_890b_08732_0031_santee_court_rear_3_4_lifestyle_004846.jpg",
+  },
+]);
 </script>
