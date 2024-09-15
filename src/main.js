47d222ba8111dce0217d9fd481cd559daf72ca89
sunghwan kwon <sunghwan.ian.kwon@gmail.com>
2024-09-15 11:45:58 +0900

    Fixed an accordion malfunction by introducing bootstrap-vue-3 to handle Bootstrap and Vue.js DOM conflicts.

--- src/main.js
+++ src/main.js
@@ -3,12 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import 'jquery';
-import 'popper.js';
-import 'bootstrap-vue-3/dist/bootstrap-vue-3.css';
-import BootstrapVue3 from 'bootstrap-vue-3';
+import BootstrapVue3 from 'bootstrap-vue-3'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
 
-createApp(App).use(store).use(router).use(BootstrapVue3).mount('#app')
+const app = createApp(App)
+
+app.use(store)
+app.use(router)
+app.use(BootstrapVue3)
+
+app.mount('#app')
