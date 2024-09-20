f44e92d3df6234b3dc347cf03d229265d4a6c240
sunghwan kwon <sunghwan.ian.kwon@gmail.com>
2024-09-20 22:58:13 +0900

    Added delete confirmation modal and implemented sorting by category

---
+++ src/views/Admin/ManageImages/Pagination.vue
@@ -0,0 +1,29 @@
+<template>
+  <nav aria-label="Page navigation" class="mt-5 pt-3">
+    <ul class="pagination justify-content-center">
+      <li class="page-item">
+        <a class="page-link" href="#">&laquo;</a>
+      </li>
+      <li class="page-item active">
+        <a class="page-link" href="#">1</a>
+      </li>
+      <li class="page-item">
+        <a class="page-link" href="#">2</a>
+      </li>
+      <li class="page-item">
+        <a class="page-link" href="#">3</a>
+      </li>
+      <li class="page-item">
+        <a class="page-link" href="#">4</a>
+      </li>
+      <li class="page-item">
+        <a class="page-link" href="#">5</a>
+      </li>
+      <li class="page-item">
+        <a class="page-link" href="#">&raquo;</a>
+      </li>
+    </ul>
+  </nav>
+</template>
+<script setup></script>
+<style scoped></style>
--- src/views/Admin/ManageImages/index.vue
+++ src/views/Admin/ManageImages/index.vue
@@ -2,78 +2,138 @@
   <div class="admin-container d-flex container-fluid p-0">
     <div class="admin-content flex-grow-1">
       <div class="manage-category">
-        <h3 class="bg-dark text-white p-3 ps-4 mb-4 fw-bold">manage Images</h3>
+        <h3 class="bg-dark text-white p-3 ps-4 mb-4 fw-bold">Manage Images</h3>
         <div class="manage-image-padding">
-          <div class="d-flex justify-content-end mb-3">
-            <button class="btn btn-primary">Create Project</button>
+          <div class="d-flex justify-content-end align-items-center">
+            <RouterLink class="btn btn-primary" to="/Admin/AdminUpload"
+              >Create Project</RouterLink
+            >
           </div>
-          <table class="table table-hover text-center align-middle">
+          <table class="table table-hover text-center align-middle mt-4">
             <thead>
               <tr>
-                <th>No</th>
-                <th>thumbnail</th>
-                <th>Project name</th>
-                <th>Category</th>
-                <th>Date</th>
-                <th>Images</th>
-                <th>Views</th>
+                <th @click="sort('no')" class="category">
+                  No
+                  <span v-if="currentSort === 'no'">{{
+                    currentSortDir === "asc" ? "↑" : "↓"
+                  }}</span>
+                </th>
+                <th class="thumbnail">Thumbnail</th>
+                <th @click="sort('projectName')" class="category">
+                  Project name
+                  <span v-show="currentSort === 'projectName'">{{
+                    currentSortDir === "asc" ? "↑" : "↓"
+                  }}</span>
+                </th>
+                <th @click="sort('category')" class="category">
+                  Category
+                  <span v-if="currentSort === 'category'">{{
+                    currentSortDir === "asc" ? "↑" : "↓"
+                  }}</span>
+                </th>
+                <th @click="sort('date')" class="category">
+                  Date
+                  <span v-if="currentSort === 'date'">{{
+                    currentSortDir === "asc" ? "↑" : "↓"
+                  }}</span>
+                </th>
+                <th @click="sort('images')" class="category">
+                  Images
+                  <span v-if="currentSort === 'images'">{{
+                    currentSortDir === "asc" ? "↑" : "↓"
+                  }}</span>
+                </th>
+                <th @click="sort('views')" class="category">
+                  Views
+                  <span v-if="currentSort === 'views'">{{
+                    currentSortDir === "asc" ? "↑" : "↓"
+                  }}</span>
+                </th>
                 <th></th>
               </tr>
             </thead>
             <tbody>
-              <tr v-for="(item, index) in items" :key="index">
+              <tr v-for="(item, index) in sortedItems" :key="index">
                 <td>{{ item.no }}</td>
                 <td>
                   <img
                     :src="item.thumbnail"
                     alt="thumbnail"
-                    style="width: 100px; height: 100px;"
+                    style="width: 100px; height: 100px"
                   />
                 </td>
                 <td>
-                  <span class="underline-text">
-                    {{ item.projectName }}
-                  </span>
+                  <span class="underline-text">{{ item.projectName }}</span>
                 </td>
-
                 <td>{{ item.category }}</td>
                 <td>{{ item.date }}</td>
                 <td>+{{ item.images }}</td>
                 <td>{{ item.views }}</td>
                 <td>
-                  <div class="d-flex flex-column justify-content-center align-items-center">
-                    <button class="btn btn-secondary img-edit-buttons btn-sm mb-1 w-50">edit</button>
-                    <button class="btn btn-danger img-edit-buttons btn-sm w-50">delete</button>
+                  <div
+                    class="d-flex flex-column justify-content-center align-items-center"
+                  >
+                    <button
+                      class="btn btn-secondary img-edit-buttons btn-sm mb-1 w-50"
+                      @click="clickEdit(item.no)"
+                    >
+                      edit
+                    </button>
+                    <button
+                      class="btn btn-danger img-edit-buttons btn-sm w-50"
+                      data-bs-toggle="modal"
+                      data-bs-target="#deleteModal"
+                      @click="clickDelete(item.no)"
+                    >
+                      delete
+                    </button>
                   </div>
                 </td>
               </tr>
             </tbody>
           </table>
-          <nav aria-label="Page navigation" class="mt-5 pt-3">
-            <ul class="pagination justify-content-center">
-              <li class="page-item">
-                <a class="page-link" href="#">&laquo;</a>
-              </li>
-              <li class="page-item active">
-                <a class="page-link" href="#">1</a>
-              </li>
-              <li class="page-item">
-                <a class="page-link" href="#">2</a>
-              </li>
-              <li class="page-item">
-                <a class="page-link" href="#">3</a>
-              </li>
-              <li class="page-item">
-                <a class="page-link" href="#">4</a>
-              </li>
-              <li class="page-item">
-                <a class="page-link" href="#">5</a>
-              </li>
-              <li class="page-item">
-                <a class="page-link" href="#">&raquo;</a>
-              </li>
-            </ul>
-          </nav>
+          <Pagination />
+        </div>
+      </div>
+    </div>
+
+    <!-- Delete Modal -->
+    <div
+      class="modal fade"
+      id="deleteModal"
+      tabindex="-1"
+      aria-labelledby="deleteModalLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h1 class="modal-title fs-5" id="edeleteModalLabel">Delete</h1>
+            <button
+              type="button"
+              class="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></button>
+          </div>
+          <div class="modal-body">Are you sure you want to delete this project?</div>
+          <div class="modal-footer">
+            <button
+              type="button"
+              class="btn btn-secondary"
+              data-bs-dismiss="modal"
+            >
+              Cancel
+            </button>
+            <button
+              type="button"
+              class="btn btn-danger"
+              data-bs-dismiss="modal"
+              @click="clickDeleteModal"
+            >
+              Delete
+            </button>
+          </div>
         </div>
       </div>
     </div>
@@ -81,49 +141,96 @@
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref, computed } from "vue";
+import Pagination from "./Pagination.vue";
+
+const currentItemNo = ref(0);
+const currentSort = ref("");
+const currentSortDir = ref("asc");
 
 const items = ref([
-  { 
-    no: 41, 
-    thumbnail: 'https://www.summahealth.org/-/media/project/summahealth/website/page-content/flourish/2_18a_fl_fastfood_400x400.webp?la=en&h=400&w=400&hash=145DC0CF6234A159261389F18A36742A', 
-    projectName: '프로젝트 이름', 
-    category: 'Food', 
-    date: '2022.12.05', 
-    images: 7, 
-    views: 5
+  {
+    no: 41,
+    thumbnail:
+      "https://www.summahealth.org/-/media/project/summahealth/website/page-content/flourish/2_18a_fl_fastfood_400x400.webp?la=en&h=400&w=400&hash=145DC0CF6234A159261389F18A36742A",
+    projectName: "프로젝트 이름1",
+    category: "Food",
+    date: "2022.12.05",
+    images: 7,
+    views: 5,
   },
-  { 
-    no: 40, 
-    thumbnail: 'https://static.wtable.co.kr/image/production/service/recipe/1718/531d4f4a-f370-40ba-b7d3-e844f356ee5d.jpg', 
-    projectName: '프로젝트 이름', 
-    category: 'Portraits', 
-    date: '2022.12.05', 
-    images: 6, 
-    views: 3 
+  {
+    no: 40,
+    thumbnail:
+      "https://static.wtable.co.kr/image/production/service/recipe/1718/531d4f4a-f370-40ba-b7d3-e844f356ee5d.jpg",
+    projectName: "프로젝트 이름2",
+    category: "Portraits",
+    date: "2022.12.05",
+    images: 6,
+    views: 3,
   },
-  { 
-    no: 39, 
-    thumbnail: 'https://ssproxy.ucloudbiz.olleh.com/v1/AUTH_e59809eb-bdc9-44d7-9d8f-2e7f0e47ba91/store16/88b344473efdb0d217f35b22e67194ab_731x688.JPG', 
-    projectName: '프로젝트 이름', 
-    category: 'Street', 
-    date: '2022.12.05', 
-    images: 12, 
-    views: 4 
+  {
+    no: 39,
+    thumbnail:
+      "https://ssproxy.ucloudbiz.olleh.com/v1/AUTH_e59809eb-bdc9-44d7-9d8f-2e7f0e47ba91/store16/88b344473efdb0d217f35b22e67194ab_731x688.JPG",
+    projectName: "프로젝트 이름3",
+    category: "Street",
+    date: "2022.12.05",
+    images: 12,
+    views: 4,
   },
-  { 
-    no: 38, 
-    thumbnail: 'https://img.choroc.com/newshop/goods/009179/009179_1.jpg', 
-    projectName: '프로젝트 이름', 
-    category: 'Food', 
-    date: '2022.12.05', 
-    images: 16, 
-    views: 2 
+  {
+    no: 38,
+    thumbnail: "https://img.choroc.com/newshop/goods/009179/009179_1.jpg",
+    projectName: "프로젝트 이름4",
+    category: "Food",
+    date: "2022.12.05",
+    images: 16,
+    views: 2,
   },
 ]);
+
+const sort = (s) => {
+  if (s === currentSort.value) {
+    currentSortDir.value = currentSortDir.value === "asc" ? "desc" : "asc";
+  }
+  currentSort.value = s;
+};
+
+const sortedItems = computed(() => {
+  return [...items.value].sort((a, b) => {
+    let modifier = 1;
+    if (currentSortDir.value === "desc") modifier = -1;
+    if (a[currentSort.value] < b[currentSort.value]) return -1 * modifier;
+    if (a[currentSort.value] > b[currentSort.value]) return 1 * modifier;
+    return 0;
+  });
+});
+
+function clickEdit(index) {
+  console.log("수정 index: " + index);
+}
+function clickDelete(index) {
+  currentItemNo.value = index;
+}
+function clickDeleteModal() {
+  console.log("삭제하려는 프로젝트 값: " + currentItemNo.value);
+
+  //axios 통신 코드 작성 자리
+
+  currentItemNo.value = 0;
+}
 </script>
 
 <style scoped>
+.table > thead > tr > .category {
+  cursor: pointer;
+}
+
+.table > thead > tr > .thumbnail {
+  cursor: default;
+}
+
 .manage-image-padding {
   padding: 40px;
 }
@@ -134,8 +241,8 @@ const items = ref([
 
 .underline-text {
   display: inline-block;
-  border-bottom: 1px solid #000; 
-  padding-bottom: 4px; /* 글자와 밑줄 사이의 간격을 조절 */
+  border-bottom: 1px solid #000;
+  padding-bottom: 4px;
 }
 
 </style>
